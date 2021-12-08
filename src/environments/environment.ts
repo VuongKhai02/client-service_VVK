27c1a664c95aeb1a717c9a78af29d0e56a874dff
ecyberlinh <ecyberlinh@gmail.com>
2021-12-08 23:02:50 +0700

    linhnv10

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -19,7 +19,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  appTitle: 'Facnet IOT Platform', 
+  appTitle: 'Nền tảng IOT platform', 
   production: false,
 // @ts-ignore
   tbVersion: TB_VERSION,
